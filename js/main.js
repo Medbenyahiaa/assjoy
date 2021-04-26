09e95cea3de19e7747b085c1982ad0ea8455ecd3
Medbenyahiaa <mohamedbenyahia.mk@gmail.com>
2021-04-26 20:57:43 +0000

    first commit

--- js/main.js
+++ js/main.js
@@ -59,3 +59,39 @@ $(document).ready(function () {
         showCloseButton: true,
     });
 });
+
+$("#who-li").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $(".presentation").offset().top,
+        },
+        1500
+    );
+});
+
+$("#actions-li").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $(".posts").offset().top,
+        },
+        1500
+    );
+});
+
+$("#members-li").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $(".members").offset().top,
+        },
+        1500
+    );
+});
+
+$("#contacts-li").click(function () {
+    $("html, body").animate(
+        {
+            scrollTop: $(".about-us").offset().top,
+        },
+        1500
+    );
+});
